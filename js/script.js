28c86653a93d978673ecaef0e9124ad9b1bfdd97
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-04 18:41:27 +0200

    condizioni

--- js/script.js
+++ js/script.js
@@ -18,6 +18,43 @@ console.log(scontoMin);
 const scontoOver = (treno * prezzo) * 40 / 100;
 console.log(scontoOver);
 
+
+/*
+if ( condizione booleano ) {
+    //fai se vero
+    //fai se vero
+    //fari se vero
+} else {
+   //fai se fals
+    //fai se falso
+    //fari se falso
+}
+
+// sconto del 20% per i minori
+// sconto del 40% per gli over 65
+//if annidato
+if ( condizione 1 ) {
+    //fai se vero cond1
+
+} else if ( condizione 2) {
+   //fai se vero cond 2
+
+} else {
+    //fai negli altri casi
+    // fai quando cond 1 e cond2 sono entrambe false
+}
+*/
+
+if (eta < 18) {
+//sconto 20% --> andiamo a riga 58 (salto)
+    risultato = scontoMin;
+} else if (eta > 65) {
+// sconto 40%
+    risultato = scontoOver;
+} else {
+// no sconto
+}
+
 // Prezzo finale del biglietto 
-document.getElementById(mio_id).innerHTML
+document.getElementById(mio_id).innerHTML = prezzo;
 
